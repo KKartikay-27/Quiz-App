07ad695ba7cd0a438bf3adb2b4e5a958bb6197fa
kumarkartikay <kumarkartikay005@gmail.com>
2024-05-21 19:35:40 +0530

    initial commit

---
+++ questions.js
@@ -0,0 +1,26 @@
+let questions = [   
+    {
+        question: 'Which HTML tag is used to define an inline style?',
+        1: '<script>',
+        2: '<css>',
+        3: '<style>',
+        4: '<span>',
+        answer: 3,
+    },
+    {
+        question: 'Which property is used to change the text color in CSS?',
+        1: 'text-color',
+        2: 'font-color',
+        3: 'text-style',
+        4: 'color',
+        answer: 4,
+    },
+    {
+        question: 'Which of the following is the correct way to comment in HTML?',
+        1: '// Comment',
+        2: '<!-- Comment -->',
+        3: '/* Comment */',
+        4: '<! Comment>',
+        answer: 2,
+    },
+];
